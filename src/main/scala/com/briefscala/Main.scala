b14d3be87a84515e5aea25c3bdf56e468d3c3758
Yoel Garcia Diaz <yoel.garciadiaz@razorfish.com>
2016-03-06 20:25:22 -0500

    add main example

--- src/main/scala/com/briefscala/Main.scala
+++ src/main/scala/com/briefscala/Main.scala
@@ -1,8 +1,37 @@
 package com.briefscala
 
+import com.typesafe.config.ConfigFactory
+import com.briefscala.recparse._
+import scalaz._, Scalaz._
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("hello")
+
+    val config = ConfigFactory.load()
+    val separator = config.getString("rec-parse.separator")
+
+    val args0 = args.flatMap(_.split("="))
+
+    val args1 = args0.toSeq
+
+    val maybeFilePath = args1.getRecord[FilePath]
+    val maybeSource = args1.getRecord[IsNew]
+    val maybeLang = args1.getRecord[Len]
+    val maybeSeparator = args1.getRecord[Separator]
+      .orElse(Seq("-sep", separator).getRecord[Separator])
+
+    val failedFilePath = "No file path was specified or was invalid".failureNel[FilePath]
+    val failedSeparator = "An invalid separator was specified".failureNel[Separator]
+    val failedLength = "No lenth specified or was invalid".failureNel[Len]
+    val failedIsNew = "No --is-new flag specified or was invalid".failureNel[IsNew]
+
+    val argsValidation = (
+      maybeFilePath.fold(failedFilePath)(_.successNel) |@|
+      maybeSeparator.fold(failedSeparator)(_.successNel) |@|
+      maybeLang.fold(failedLength)(_.successNel) |@|
+      maybeSource.fold(failedIsNew)(_.successNel)
+    ){_++_++_++_}
+
+    println(argsValidation)
   }
 }
-
