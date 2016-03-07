ffe1996c98a737e3bb69bbda9e0fb918c55c8598
Yoel Garcia Diaz <yoel.garciadiaz@razorfish.com>
2016-03-07 00:44:26 -0500

    rename params

--- src/main/scala/com/briefscala/Main.scala
+++ src/main/scala/com/briefscala/Main.scala
@@ -27,14 +27,14 @@ object Main {
       * try to parse in the records from the provided arguments
       */
      val maybeFilePath = args1.getRecord[FilePath]
-     val maybeSource = args1.getRecord[IsNew]
-     val maybeLang = args1.getRecord[Len]
+     val maybeInNew = args1.getRecord[IsNew]
+     val maybeLen = args1.getRecord[Len]
      val maybeSeparator = args1.getRecord[Separator]
        .orElse(Seq("-sep", separator).getRecord[Separator])
 
      val failedFilePath = "No file path was specified".failureNel[FilePath]
      val failedSeparator = "An invalid separator was specified".failureNel[Separator]
-     val failedLength = "No lenth specified".failureNel[Len]
+     val failedLen = "No lenth specified".failureNel[Len]
      val failedIsNew = "No --is-new flag specified".failureNel[IsNew]
 
      /**
@@ -43,8 +43,8 @@ object Main {
      val argsValidation = (
        maybeFilePath.fold(failedFilePath)(_.successNel) |@|
        maybeSeparator.fold(failedSeparator)(_.successNel) |@|
-       maybeLang.fold(failedLength)(_.successNel) |@|
-       maybeSource.fold(failedIsNew)(_.successNel)
+       maybeLen.fold(failedLen)(_.successNel) |@|
+       maybeInNew.fold(failedIsNew)(_.successNel)
      ){_++_++_++_}
 
      argsValidation match {
